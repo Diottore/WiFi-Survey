097dd6bd0258c86b108d3f72ec639631a990d684
Diego de Jesus Patiño Suarez <diego.djps@gmail.com>
2025-10-27 08:54:09 -0400

    Create utils.js for common utility functions

---
+++ static/utils.js
@@ -0,0 +1,16 @@
+// utils.js
+
+// Common utility functions
+
+function calculateStats(data) {
+    // Implementation for calculating stats
+}
+
+function formatSummary(summary) {
+    // Implementation for formatting summary
+}
+
+module.exports = {
+    calculateStats,
+    formatSummary,
+};
